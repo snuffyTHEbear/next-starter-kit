9384b07e37925f976564b78e7a230b0961b1fca6
Robert Daniels <rob.daniels3@gmail.com>
2024-07-31 22:20:38 +0100

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='mt-4 text-3xl font-bold text-center'>
+          Welcome to Cutsy Cuts
+        </h1>
+      </div>
+    </section>
+  )
+}
